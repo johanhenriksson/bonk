ca5f7591f9476a8d1f08c11a39e2a861bbfa159f
Michal Stypa <michal@backtick.se>
2024-04-08 12:36:53 +0200

    feat: added wholesome sound on big boy ranged crit spell kills

--- Bonk.lua
+++ Bonk.lua
@@ -1,26 +1,61 @@
-local function bonk()
-	PlaySoundFile("Interface\\AddOns\\Bonk\\sfx\\bonk.ogg")
+-- them sounds
+local sounds = {
+    PLAYER_KILL = {
+        spellCrit = "Interface\\AddOns\\Bonk\\sfx\\awp.ogg",
+        normal = "Interface\\AddOns\\Bonk\\sfx\\bonk.ogg"
+    },
+}
+
+local function playSound(soundFilePath)
+    if soundFilePath then
+        PlaySoundFile(soundFilePath)
+    end
 end
 
+function eventDestinationIsAnotherPlayer(destGUID)
+    local firstPart = strsplit("-", destGUID)[1]
+    return firstPart == "Player"
+end
+
+function isPartyKill(event)
+    return event == "PARTY_KILL"
+end
+
+function isRangedSpellDamage(spellId)
+    local _, _, spellSchool = GetSpellInfo(spellId)
+
+    -- Spell schools: 1 = Physical, 2 = Holy, 4 = Fire, 8 = Nature, 16 = Frost, 32 = Shadow, 64 = Arcane
+    -- Combining spell schools indicates a multi-school spell. For simplicity, this example checks for pure schools.
+    -- Note: This is a simplified approach and might not accurately classify all ranged damage correctly.
+    local rangedSchools = {
+        [4] = true,  -- Fire
+        [8] = true,  -- Nature
+        [16] = true, -- Frost
+        [32] = true, -- Shadow
+        [64] = true, -- Arcane
+    }
+
+    return rangedSchools[spellSchool] or false
+end
+
+
 local function CombatLogEventHandler(self)
-	local _, event, _, _, s_name, _, _, d_guid, d_name, _, _ = CombatLogGetCurrentEventInfo()
-	if event ~= "PARTY_KILL" then
-		return
-	end
+    local _, event, _, _, s_name, _, _, destGUID, _, _, _, spellId, _, _, _, overkill, _, _, _, _, critical = CombatLogGetCurrentEventInfo()
+    
+    -- if not me, return
+    if s_name ~= UnitName("player") then return end
+    
+    
+    if isPartyKill(event) and eventDestinationIsAnotherPlayer(destGUID) then
+        local soundToPlay = sounds.PLAYER_KILL.normal
+        
+        if critical and (overkill and overkill > 0) and isRangedSpellDamage(spellId) then
+            soundToPlay = sounds.PLAYER_KILL.spellCrit
+        end
 
-	local me = UnitName("player")
-	if s_name ~= me then
-		-- wasnt me
-		return
-	end
+        playSound(soundToPlay)
+    end
 
-	local info = GetPlayerInfoByGUID(d_guid)
-	if info == nil then
-		-- wasnt a player
-		return
-	end
-
-	bonk()
 end
 
 local EventFrame = CreateFrame("frame", "EventFrame")
